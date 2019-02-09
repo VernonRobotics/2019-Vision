a14e5a978ef6256ef3aab5b792cdb7be6ce7b455
MattThienel <matthew.thienel@gmail.com>
2019-02-09 10:56:28 -0500

    Test for network tables with line detection

--- line_detection.py
+++ line_detection.py
@@ -2,63 +2,67 @@ import cv2
 import numpy as np
 import math
 
-image = cv2.imread('Test_Images/image5.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture(0)
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
 
-M = np.ones(gray.shape, dtype= "uint8") * 10
-gray = cv2.subtract(gray, M)
+while(True):
+    ret, frame = cap.read()
 
-ret, threshold = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+    image = frame
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-kernel = np.ones((5,5), np.uint8)
-
-opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
-
-#cv2.imshow("Opening", opening)
-height, width = opening.shape[:2]
-opening = cv2.rectangle(opening, (0,0), (width, 10), (0,0,0), -1)
-opening = cv2.rectangle(opening, (0, height-10), (width, height), (0,0,0), -1)
+    M = np.ones(gray.shape, dtype= "uint8") * 10
+    gray = cv2.subtract(gray, M)
 
 
-#edged = cv2.Canny(opening, 30, 200)
+    ret, threshold = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
 
-_, contours, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    kernel = np.ones((5,5), np.uint8)
 
-sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-c = sorted_contours[0]
-rows, cols = image.shape[:2]
-[vx,vy,x,y] = cv2.fitLine(c, cv2.DIST_L2,0,0.01,0.01)
-lefty = int((-x*vy/vx) + y)
-righty = int(((cols-x)*vy/vx)+y)
-#print vx, vy, x, y
-#print (cols-1,righty), (0,lefty)
-slope = (float)(righty-lefty)/(cols-1)
-coord1 = (int(((-1 * lefty)/slope)),0)
-coord2 = (int((((-1 * lefty)+height)/slope)),height)
-cv2.line(image,(cols-1,righty),(0,lefty),(0,0,255),2)
-cv2.line(image, coord1, coord2, (255,0,0), 3)
-#cv2.imshow("Largest Contour", image)
-
+    opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
     
-def getPointsForRobotControl():
-    x1,y1 = coord1[0],coord1[1]
-    x2, y2 = coord2[0], coord2[1]
-    angle = 0.0
-    x1 = (float)(x1-(width/2))/(width/2)
-    y1 = (float)(y1-(height/2))/(height/2)
-    x2 = (float)(x2-(width/2))/(width/2)
-    y2 = (float)(y2-(height/2))/(height/2)
+    height, width = opening.shape[:2]
+    opening = cv2.rectangle(opening, (0,0), (width, 10), (0,0,0), -1)
+    opening = cv2.rectangle(opening, (0, height-10), (width, height), (0,0,0), -1)
 
-    angle = math.atan((x1-x2)/(y1-y2))
-    x = (x1+x2)/2
-    angle = round(angle,5)
-    x = round(x, 5)
-    return x, angle
 
-#print getPointsForRobotControl()
+    contours, hierarchy = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    
+    try:
+        sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
-#cv2.imshow("Opening", opening)
-#cv2.imshow("Contours", image)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+        c = sorted_contours[0]
+        rows, cols = image.shape[:2]
+        [vx,vy,x,y] = cv2.fitLine(c, cv2.cv.CV_DIST_L2,0,0.01,0.01)
+        lefty = int((-x*vy/vx) + y)
+        righty = int(((cols-x)*vy/vx)+y)
+        slope = (float)(righty-lefty)/(cols-1)
+        coord1 = (int(((-1 * lefty)/slope)),0)
+        coord2 = (int((((-1 * lefty)+height)/slope)),height)
+        cv2.line(image,(cols-1,righty),(0,lefty),(0,0,255),2)
+        cv2.line(image, coord1, coord2, (255,0,0), 3)
+    except:
+        print("line calculation error")
+
+        
+    def getPointsForRobotControl():
+        x1,y1 = coord1[0],coord1[1]
+        x2, y2 = coord2[0], coord2[1]
+        angle = 0.0
+        x1 = (float)(x1-(width/2))/(width/2)
+        y1 = (float)(y1-(height/2))/(height/2)
+        x2 = (float)(x2-(width/2))/(width/2)
+        y2 = (float)(y2-(height/2))/(height/2)
+        
+        angle = math.atan((x1-x2)/(y1-y2))
+        x = (x1+x2)/2
+        angle = round(angle,5)
+        x = round(x,5)
+        return x, angle
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
--- networktable_test.py
+++ networktable_test.py
@@ -3,6 +3,10 @@ import time
 from networktables import NetworkTables
 #from line_detection import getPointsForRobotControl
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 cond = threading.Condition()
 notified = [False]
 
@@ -25,25 +29,26 @@ print("Connected")
 vis_T = NetworkTables.getTable("VisionTable")
 robot_state = vis_T.getAutoUpdateValue("RobotState", "disabled")
 counter = 0
-def vision_processing():
-    while True:
-        '''y, angle = getPointsForRobotControl()
-        vis_T.putNumber("Y", y)
-        vis_T.putNumber("Angle", angle)'''
-        counter = counter + 1
-        vis_T.putNumber("Y", counter)
-
-        time.sleep(.4)
+#def vision_processing():
+while True:
+    y, angle = getPointsForRobotControl()
+    vis_T.putNumber("Y", y)
+    vis_T.putNumber("Angle", angle)
+    '''counter = counter + 1
+    vis_T.putNumber("Y", counter)'''
+    print(robot_state.value)
+    time.sleep(.4)
 
 #def camera_feed():
 
-vis_thread = threading.Thread(name='vision_thread', target=vision_processing)
+'''vis_thread = threading.Thread(name='vision_thread', target=vision_processing)
 vis_thread.setDaemon(True)
-
+vis_thread.start()'''
 #cam_thread = threading.Thread(name="camera_feed", target=camera_thread)
 #cam_thread.setDaemon(True)
 
 
-if not robot_state.value == "disabled":
-    vis_thread.start()
+'''if not robot_state.value == "disabled":
+    print robot_state.value
+    vis_thread.start()'''
     #cam_thread.start()
